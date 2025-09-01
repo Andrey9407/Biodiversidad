7e3de46cf1cfbbab1cd83108be022e854a506888
Andrey9407 <brayan_andrey94@hotmail.com>
2025-09-01 18:11:56 -0500

    pagina de prueba cargue 1

---
+++ assets/js/region.js
@@ -0,0 +1,98 @@
+var tituloetq = document.getElementById("tituloregion")
+var imagentq = document.getElementById("imagenregion")
+var infoetq = document.getElementById("info")
+function eventoguavio() {
+    titulo = "Región Guavio"
+    imagen = "assets/img/region/guavio.jpg"
+    info = "Localizada al oriente del departamento, es de gran importancia por hallarse sus grandes reservas forestales e hídricas situadas en su mayor parte en la cuenca del Río Meta (Ríos Guavio, Sucio, Moquentivá, Chorreras, Blanco y Concepción) una mínima parte en la del Magdalena (concretamente en la cuenca Río Bogotá como el Teusacá y el Tominé), así como lagunas (algunas con potencial hidroeléctrico) como San Rafael, El Sapo, Siecha, Guatavita, Guavio y Tominé y el Parque nacional natural Chingaza (declarado patrimonio hídrico de la humanidad con el título de Humedales Ramsar)."
+    eventoregion(titulo, imagen, info)
+}
+function sabanacentro() {
+    titulo = "Región Sabana Centro"
+    imagen = "assets/img/region/zipaquira.png"
+    info = "Localizada al centro del departamento, es recorrida de norte a sur por el río Bogotá. En su territorio existen minas de sal en los municipios de Nemocón, Sesquilé y Zipaquirá; siendo la de Zipaquirá la más conocida, ambas minas están dedicadas al turismo. El territorio de la actual provincia fue poblado en la época prehispánica por los indígenas Muiscas. Gran parte del territorio se encuentra sobre la Sabana de Bogotá.."
+    eventoregion(titulo, imagen, info)
+}
+function ubate() {
+    titulo = "Región Ubate"
+    imagen = "assets/img/region/ubate.png"
+    info = "Ubaté, oficialmente Villa de San Diego de Ubaté, es un municipio colombiano del departamento de Cundinamarca ubicado en la Provincia de Ubaté, de la que es capital. Se encuentra en el centro del valle de Ubaté, a 95 km al nororiente de Bogotá, a 50 km de Chiquinquirá, a 45 km de Zipaquirá, 315 km de Bucaramanga y 95 km de Muzo. Ubaté es conocido como la Capital Lechera de Colombia. Tiene una población de 45 000 habitantes para 2019."
+    eventoregion(titulo, imagen, info)
+}
+function bajoMagdalena() {
+    titulo = "Región Bajo Magdalena"
+    imagen = "assets/img/region/bajoMagdalena.jpg"
+    info = "El Bajo Magdalena es una subregión de Cundinamarca, ubicada en la zona noroccidental del departamento, a lo largo del río Magdalena. Esta región es conocida por sus tierras fértiles, lo que la convierte en un área importante para la agricultura, con cultivos de arroz, maíz, yuca y otros productos de clima cálido. Además, el Bajo Magdalena tiene una fuerte tradición pesquera debido a la proximidad al río, que es una fuente vital de sustento para las comunidades locales. Los municipios de esta región, como Girardot, Guaduas y Honda, también tienen un legado histórico y cultural significativo, con un pasado vinculado al comercio fluvial y a la colonización española. La región es igualmente apreciada por su turismo, especialmente por sus balnearios y actividades acuáticas en torno al río Magdalena."
+    eventoregion(titulo, imagen, info)
+}
+function rionegro() {
+    titulo = "Región Rionegro"
+    imagen = "assets/img/region/rionegro.jpg"
+    info = "La región del Rionegro en Cundinamarca está ubicada en la parte suroccidental del departamento y es una zona rica en recursos naturales y biodiversidad. Esta subregión toma su nombre del río Negro, un afluente importante del río Magdalena, que atraviesa gran parte del territorio y es fundamental para la agricultura y la vida diaria de sus habitantes. El clima de la región es variado, predominando los climas templado y cálido, lo que favorece el cultivo de productos como café, frutas, caña de azúcar y maíz. Municipios como Pacho, La Palma y Yacopí son representativos de la zona, donde también se encuentran tradiciones culturales y festividades locales que reflejan la identidad de sus comunidades. Además, el Rionegro es una región con potencial turístico debido a sus paisajes naturales, ríos, y montañas que atraen a visitantes interesados en ecoturismo y actividades al aire libre."
+    eventoregion(titulo, imagen, info)
+}
+function gualiva() {
+    titulo = "Región Gualivá"
+    imagen = "assets/img/region/gualiva.jpg"
+    info = "La región de Gualivá, situada en la parte occidental de Cundinamarca, es una subregión caracterizada por su riqueza hídrica y paisajes montañosos. Esta área es atravesada por importantes ríos como el río Villeta y el río Sucio, que no solo son fundamentales para la agricultura local, sino también para la generación de energía hidroeléctrica. Gualivá cuenta con un clima predominantemente cálido, lo que permite el cultivo de productos como caña de azúcar, café, plátano y diversas frutas tropicales. Además, municipios como Villeta, La Vega y Nimaima son conocidos por sus atractivos turísticos, incluidos balnearios naturales, cascadas y senderos ecológicos, que atraen tanto a visitantes locales como internacionales. La región también posee una infraestructura en desarrollo, con un enfoque creciente en el turismo sostenible, buscando equilibrar el desarrollo económico con la preservación de su entorno natural."
+    eventoregion(titulo, imagen, info)
+}
+function almeidas() {
+    titulo = "Región Almeidas"
+    imagen = "assets/img/region/almeidas.png"
+    info = "La región de Almeidas en Cundinamarca se encuentra en la parte nororiental del departamento, y es conocida por sus paisajes montañosos y su rica tradición histórica y cultural. Esta subregión, que incluye municipios como Chocontá, Tibiritá, y Machetá, se caracteriza por su clima frío debido a su altitud, lo que favorece el cultivo de papa, maíz, trigo y otros productos de clima templado. Almeidas tiene una gran importancia histórica, ya que varias de sus localidades fueron fundadas durante la época colonial y conservan una arquitectura que refleja su pasado, con iglesias y casas antiguas de estilo colonial. Además, la región cuenta con recursos naturales valiosos, incluyendo reservas forestales y fuentes de agua que abastecen a la región y a parte de Bogotá. El turismo en la región de Almeidas también se ha ido desarrollando, con un enfoque en el ecoturismo y la visita a sitios históricos y naturales."
+    eventoregion(titulo, imagen, info)
+}
+function magdalenaCentro() {
+    titulo = "Región Magdalena Centro"
+    imagen = "assets/img/region/magdalenCentro.jpg"
+    info = "La región de Magdalena Centro, ubicada en la parte central de Cundinamarca, es una subregión que se caracteriza por su posición estratégica a lo largo del río Magdalena, lo que ha sido clave para su desarrollo económico y social. Esta área incluye municipios como Honda, La Dorada, y Puerto Salgar, los cuales han sido históricamente importantes centros comerciales y de transporte fluvial. El clima de Magdalena Centro varía desde cálido hasta templado, lo que permite una agricultura diversa, incluyendo cultivos de arroz, maíz, caña de azúcar y frutas tropicales. Además, la ganadería es una actividad económica significativa en la región."
+    eventoregion(titulo, imagen, info)
+}
+function sabanaOccidente() {
+    titulo = "Región Sabana Occidente"
+    imagen = "assets/img/region/sabanaOccidente.jpg"
+    info = "La región de Sabana Occidente en Cundinamarca se encuentra en la parte occidental de la Sabana de Bogotá y es una de las áreas más dinámicas y en crecimiento del departamento. Esta subregión incluye municipios como Mosquera, Funza, Madrid, y Facatativá, que son conocidos por su desarrollo industrial, agrícola, y residencial. Sabana Occidente es una zona de transición entre el área rural y la expansión urbana de Bogotá, lo que ha llevado a un rápido crecimiento en infraestructura, con la construcción de parques industriales, zonas francas, y urbanizaciones. El clima de Sabana Occidente es templado, lo que favorece la agricultura intensiva, especialmente en el cultivo de flores, hortalizas, y cereales, que son exportados a nivel nacional e internacional. Además, la región cuenta con una rica historia, evidenciada en sitios arqueológicos como el Parque Arqueológico Piedras del Tunjo en Facatativá, que conserva vestigios de la cultura muisca."
+    eventoregion(titulo, imagen, info)
+}
+function altoMagdalena() {
+    titulo = "Región Alto Magdalena"
+    imagen = "assets/img/region/altoMagdalena.jpg"
+    info = "La región del Alto Magdalena en Cundinamarca se encuentra en la parte suroccidental del departamento, abarcando un área que sigue el curso del río Magdalena en su tramo alto. Esta subregión es conocida por su clima cálido y su gran biodiversidad, lo que la convierte en un importante centro agrícola y turístico. Municipios como Girardot, Ricaurte, Tocaima y Nariño forman parte de esta región, caracterizada por sus fértiles tierras que son ideales para el cultivo de productos como algodón, maíz, plátano, y frutas tropicales."
+    eventoregion(titulo, imagen, info)
+}
+function tequendama() {
+    titulo = "Región Tequendama"
+    imagen = "assets/img/region/tequendama.jpg"
+    info = "La región de Tequendama en Cundinamarca, situada al suroeste de Bogotá, es conocida por su impresionante riqueza natural y su historia ancestral. Esta subregión incluye municipios como Soacha, San Antonio del Tequendama, y Tena, y se caracteriza por su variado clima, que va desde el frío en las zonas altas hasta el cálido en los valles. Uno de sus mayores atractivos es el Salto del Tequendama, una majestuosa cascada de 157 metros que ha sido un símbolo de la región y un sitio de importancia cultural para los pueblos indígenas muiscas. Además de su belleza natural, Tequendama es una región agrícola productiva, con cultivos de café, frutas, y flores, y también es un área de creciente desarrollo residencial y turístico debido a su cercanía a la capital."
+    eventoregion(titulo, imagen, info)
+}
+function sumapaz() {
+    titulo = "Región Sumapaz"
+    imagen = "assets/img/region/sumapaz.jpg"
+    info = "La región de Sumapaz en Cundinamarca, ubicada en la parte sur del departamento, es conocida por albergar el majestuoso Páramo de Sumapaz, el páramo más grande del mundo, que es una fuente vital de agua para gran parte de Colombia. Esta subregión incluye municipios como Cabrera, Pasca, y Arbeláez, y se caracteriza por su clima frío debido a su altitud, lo que crea un entorno ideal para la conservación de ecosistemas únicos y especies endémicas. Sumapaz es también una región agrícola importante, con cultivos de papa, maíz y frutales, que se desarrollan en sus fértiles tierras. Además, su riqueza natural y paisajística, junto con la tranquilidad de sus entornos rurales, la convierten en un destino atractivo para el ecoturismo y las actividades al aire libre."
+    eventoregion(titulo, imagen, info)
+}
+function soacha () {
+    titulo = "Región soacha"
+    imagen = "assets/img/region/soacha.jpg"
+    info = "La región de Soacha en Cundinamarca, ubicada al suroeste de Bogotá, es una de las áreas más urbanizadas y pobladas del departamento, desempeñando un papel crucial como zona de expansión de la capital. Soacha, que es tanto un municipio como una región, se caracteriza por su rápido crecimiento urbano y su importancia histórica, siendo un sitio de relevancia arqueológica con vestigios de la cultura muisca. A pesar de su desarrollo urbano, Soacha mantiene áreas rurales donde se realizan actividades agrícolas, especialmente el cultivo de papa, maíz y hortalizas. La región enfrenta desafíos como la integración de su población en la dinámica metropolitana de Bogotá y el desarrollo de infraestructura, pero también tiene un potencial significativo para el crecimiento económico y social, con proyectos orientados a mejorar la calidad de vida de sus habitantes."
+    eventoregion(titulo, imagen, info)
+}
+function oriente () {
+    titulo = "Región Oriente"
+    imagen = "assets/img/region/oriente.jpg"
+    info = "La región de Oriente en Cundinamarca, situada al este del departamento, es una zona caracterizada por su riqueza natural, paisajes montañosos y climas que varían desde templado hasta frío, lo que permite una diversidad agrícola notable. Municipios como La Calera, Choachí, y Fómeque son parte de esta región, destacándose por la producción de productos como papa, maíz, hortalizas, y frutas, además de su ganadería. Oriente es también conocida por su cercanía al Parque Nacional Natural Chingaza, una importante reserva de biodiversidad que abastece de agua a gran parte de Bogotá y de sus alrededores. Además, su belleza escénica y sus aguas termales, como las de Choachí, han impulsado el ecoturismo y el turismo rural, atrayendo a visitantes que buscan disfrutar de la tranquilidad y el contacto con la naturaleza. La región de Oriente combina su riqueza agrícola con un creciente desarrollo turístico, siendo un área clave para la conservación y el bienestar ambiental en Cundinamarca."
+    eventoregion(titulo, imagen, info)
+}
+function medina () {
+    titulo = "Región Medina"
+    imagen = "assets/img/region/medina.jpg"
+    info = "La región de Medina en Cundinamarca, ubicada en la parte oriental del departamento, es una zona caracterizada por su clima cálido y su transición entre las montañas de la Cordillera Oriental y las llanuras del piedemonte llanero. El municipio de Medina es el centro de esta subregión, conocido por su actividad agrícola, con cultivos de arroz, maíz, y yuca, además de la ganadería extensiva. La región se destaca por su belleza natural, con paisajes que incluyen ríos, cascadas y selvas, lo que la convierte en un destino atractivo para el ecoturismo y la aventura. Medina también tiene un legado cultural significativo, con tradiciones que reflejan la mezcla entre la cultura andina y la llanera. Aunque es una región más aislada en comparación con otras partes de Cundinamarca, Medina ofrece un entorno natural único y un estilo de vida tranquilo, siendo un lugar ideal para quienes buscan conectar con la naturaleza y explorar la biodiversidad de esta área del departamento."
+    eventoregion(titulo, imagen, info)
+}
+function eventoregion(titulo, imagen, info) {
+    tituloetq.innerHTML = titulo
+    imagentq.src = imagen
+    infoetq.innerHTML = info
+}
